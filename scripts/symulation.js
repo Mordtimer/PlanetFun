a93077208191cb510f1d2c3b1aef313eb514fc81
Mordtimer <filipgab99@gmail.com>
2021-02-03 11:33:15 +0100

    Working model demo version (planets only interacting with central point)

--- scripts/symulation.js
+++ scripts/symulation.js
@@ -5,8 +5,9 @@ class Viewport {
         this.zoom = 1
     }
 
+
     GetMousePosition(event) {
-        var contextTransform =  canvas.getContext("2d").getTransform()
+        var contextTransform = canvas.getContext("2d").getTransform()
         return {
             x: event.offsetX / contextTransform.a - this.x,
             y: event.offsetY / contextTransform.d - this.y
@@ -14,7 +15,7 @@ class Viewport {
     }
 
     DragViewport(event) {
-        var contextTransform =  canvas.getContext("2d").getTransform()
+        var contextTransform = canvas.getContext("2d").getTransform()
         this.x += event.movementX / contextTransform.a
         this.y += event.movementY / contextTransform.d
     }
@@ -23,30 +24,521 @@ class Viewport {
         var context = canvas.getContext("2d")
 
         var mouseStart = viewport.GetMousePosition(event)
-    
-        if(event.deltaY > 0) {
+
+        if (event.deltaY > 0) {
             context.scale(0.5, 0.5)
             viewport.zoom *= 0.5
-        }
-        else {
+        } else {
             context.scale(2, 2)
             viewport.zoom *= 2
         }
-    
+
         var mouseEnd = viewport.GetMousePosition(event)
-    
+
         viewport.x += mouseEnd.x - mouseStart.x
         viewport.y += mouseEnd.y - mouseStart.y
-    
+
     }
 }
 
-class Object {
-    constructor(x, y, r) {
-        this.x = x
-        this.y = y
-        this.r = r
-        this.color = "#fff"
+class Vector2D {
+
+    constructor(x = 0, y = 0) {
+
+        Object.defineProperty(this, 'isVector2', { value: true });
+
+        this.x = x;
+        this.y = y;
+
+    }
+
+    get width() {
+
+        return this.x;
+
+    }
+
+    set width(value) {
+
+        this.x = value;
+
+    }
+
+    get height() {
+
+        return this.y;
+
+    }
+
+    set height(value) {
+
+        this.y = value;
+
+    }
+
+    set(x, y) {
+
+        this.x = x;
+        this.y = y;
+
+        return this;
+
+    }
+
+    setScalar(scalar) {
+
+        this.x = scalar;
+        this.y = scalar;
+
+        return this;
+
+    }
+
+    setX(x) {
+
+        this.x = x;
+
+        return this;
+
+    }
+
+    setY(y) {
+
+        this.y = y;
+
+        return this;
+
+    }
+
+    setComponent(index, value) {
+
+        switch (index) {
+
+            case 0:
+                this.x = value;
+                break;
+            case 1:
+                this.y = value;
+                break;
+            default:
+                throw new Error('index is out of range: ' + index);
+
+        }
+
+        return this;
+
+    }
+
+    getComponent(index) {
+
+        switch (index) {
+
+            case 0:
+                return this.x;
+            case 1:
+                return this.y;
+            default:
+                throw new Error('index is out of range: ' + index);
+
+        }
+
+    }
+
+    clone() {
+
+        return new this.constructor(this.x, this.y);
+
+    }
+
+    copy(v) {
+
+        this.x = v.x;
+        this.y = v.y;
+
+        return this;
+
+    }
+
+    add(v, w) {
+
+        if (w !== undefined) {
+
+            console.warn('THREE.Vector2: .add() now only accepts one argument. Use .addVectors( a, b ) instead.');
+            return this.addVectors(v, w);
+
+        }
+
+        this.x += v.x;
+        this.y += v.y;
+
+        return this;
+
+    }
+
+    addScalar(s) {
+
+        this.x += s;
+        this.y += s;
+
+        return this;
+
+    }
+
+    addVectors(a, b) {
+
+        this.x = a.x + b.x;
+        this.y = a.y + b.y;
+
+        return this;
+
+    }
+
+    addScaledVector(v, s) {
+
+        this.x += v.x * s;
+        this.y += v.y * s;
+
+        return this;
+
+    }
+
+    sub(v, w) {
+
+        if (w !== undefined) {
+
+            console.warn('THREE.Vector2: .sub() now only accepts one argument. Use .subVectors( a, b ) instead.');
+            return this.subVectors(v, w);
+
+        }
+
+        this.x -= v.x;
+        this.y -= v.y;
+
+        return this;
+
+    }
+
+    subScalar(s) {
+
+        this.x -= s;
+        this.y -= s;
+
+        return this;
+
+    }
+
+    subVectors(a, b) {
+
+        this.x = a.x - b.x;
+        this.y = a.y - b.y;
+
+        return this;
+
+    }
+
+    multiply(v) {
+
+        this.x *= v.x;
+        this.y *= v.y;
+
+        return this;
+
+    }
+
+    multiplyScalar(scalar) {
+
+        this.x *= scalar;
+        this.y *= scalar;
+
+        return this;
+
+    }
+
+    divide(v) {
+
+        this.x /= v.x;
+        this.y /= v.y;
+
+        return this;
+
+    }
+
+    divideScalar(scalar) {
+
+        return this.multiplyScalar(1 / scalar);
+
+    }
+
+    applyMatrix3(m) {
+
+        const x = this.x,
+            y = this.y;
+        const e = m.elements;
+
+        this.x = e[0] * x + e[3] * y + e[6];
+        this.y = e[1] * x + e[4] * y + e[7];
+
+        return this;
+
+    }
+
+    min(v) {
+
+        this.x = Math.min(this.x, v.x);
+        this.y = Math.min(this.y, v.y);
+
+        return this;
+
+    }
+
+    max(v) {
+
+        this.x = Math.max(this.x, v.x);
+        this.y = Math.max(this.y, v.y);
+
+        return this;
+
+    }
+
+    clamp(min, max) {
+
+        // assumes min < max, componentwise
+
+        this.x = Math.max(min.x, Math.min(max.x, this.x));
+        this.y = Math.max(min.y, Math.min(max.y, this.y));
+
+        return this;
+
+    }
+
+    clampScalar(minVal, maxVal) {
+
+        this.x = Math.max(minVal, Math.min(maxVal, this.x));
+        this.y = Math.max(minVal, Math.min(maxVal, this.y));
+
+        return this;
+
+    }
+
+    clampLength(min, max) {
+
+        const length = this.length();
+
+        return this.divideScalar(length || 1).multiplyScalar(Math.max(min, Math.min(max, length)));
+
+    }
+
+    floor() {
+
+        this.x = Math.floor(this.x);
+        this.y = Math.floor(this.y);
+
+        return this;
+
+    }
+
+    ceil() {
+
+        this.x = Math.ceil(this.x);
+        this.y = Math.ceil(this.y);
+
+        return this;
+
+    }
+
+    round() {
+
+        this.x = Math.round(this.x);
+        this.y = Math.round(this.y);
+
+        return this;
+
+    }
+
+    roundToZero() {
+
+        this.x = (this.x < 0) ? Math.ceil(this.x) : Math.floor(this.x);
+        this.y = (this.y < 0) ? Math.ceil(this.y) : Math.floor(this.y);
+
+        return this;
+
+    }
+
+    negate() {
+
+        this.x = -this.x;
+        this.y = -this.y;
+
+        return this;
+
+    }
+
+    dot(v) {
+
+        return this.x * v.x + this.y * v.y;
+
+    }
+
+    cross(v) {
+
+        return this.x * v.y - this.y * v.x;
+
+    }
+
+    lengthSq() {
+
+        return this.x * this.x + this.y * this.y;
+
+    }
+
+    length() {
+
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+
+    }
+
+    manhattanLength() {
+
+        return Math.abs(this.x) + Math.abs(this.y);
+
+    }
+
+    normalize() {
+
+        return this.divideScalar(this.length() || 1);
+
+    }
+
+    angle() {
+
+        // computes the angle in radians with respect to the positive x-axis
+
+        const angle = Math.atan2(-this.y, -this.x) + Math.PI;
+
+        return angle;
+
+    }
+
+    distanceTo(v) {
+
+        return Math.sqrt(this.distanceToSquared(v));
+
+    }
+
+    distanceToSquared(v) {
+
+        const dx = this.x - v.x,
+            dy = this.y - v.y;
+        return dx * dx + dy * dy;
+
+    }
+
+    manhattanDistanceTo(v) {
+
+        return Math.abs(this.x - v.x) + Math.abs(this.y - v.y);
+
+    }
+
+    setLength(length) {
+
+        return this.normalize().multiplyScalar(length);
+
+    }
+
+    lerp(v, alpha) {
+
+        this.x += (v.x - this.x) * alpha;
+        this.y += (v.y - this.y) * alpha;
+
+        return this;
+
+    }
+
+    lerpVectors(v1, v2, alpha) {
+
+        this.x = v1.x + (v2.x - v1.x) * alpha;
+        this.y = v1.y + (v2.y - v1.y) * alpha;
+
+        return this;
+
+    }
+
+    equals(v) {
+
+        return ((v.x === this.x) && (v.y === this.y));
+
+    }
+
+    fromArray(array, offset = 0) {
+
+        this.x = array[offset];
+        this.y = array[offset + 1];
+
+        return this;
+
+    }
+
+    toArray(array = [], offset = 0) {
+
+        array[offset] = this.x;
+        array[offset + 1] = this.y;
+
+        return array;
+
+    }
+
+    fromBufferAttribute(attribute, index, offset) {
+
+        if (offset !== undefined) {
+
+            console.warn('THREE.Vector2: offset has been removed from .fromBufferAttribute().');
+
+        }
+
+        this.x = attribute.getX(index);
+        this.y = attribute.getY(index);
+
+        return this;
+
+    }
+
+    rotateAround(center, angle) {
+
+        const c = Math.cos(angle),
+            s = Math.sin(angle);
+
+        const x = this.x - center.x;
+        const y = this.y - center.y;
+
+        this.x = x * c - y * s + center.x;
+        this.y = x * s + y * c + center.y;
+
+        return this;
+
+    }
+
+    random() {
+
+        this.x = Math.random();
+        this.y = Math.random();
+
+        return this;
+
+    }
+
+}
+
+
+class Planet {
+    constructor(position, velocity, distance, mass, radius, color, gravConst) {
+        this.position = position;
+        this.velocity = velocity;
+        this.distance = distance;
+        this.mass = mass;
+        this.radius = radius;
+        this.color = color
+            //this.dt = dt;
+        this.gravConst = gravConst
     }
 
     Draw(viewport) {
@@ -54,15 +546,47 @@ class Object {
 
         context.beginPath()
         context.arc(
-            this.x + viewport.x,
-            this.y + viewport.y,
-            this.r,
-            0,
-            2*Math.PI
-        )
+
+                this.position.x + viewport.x,
+                this.position.y + viewport.y,
+                this.radius,
+                0,
+                2 * Math.PI
+            )
+            // console.log("position:" + this.position.x + " viewport:" + viewport.x);
         context.fillStyle = this.color
         context.fill()
     }
+
+    Update(SUN, dt) {
+        console.log(SUN.position.x + " y: " + SUN.position.y + " dt: " +
+            dt + "gravConst: " + this.gravConst)
+        console.log("Position before:" + this.position.x + " velocity:" + this.velocity.x);
+        this.position.x = this.position.x + (this.velocity.x * dt); //parseFloat(this.position.x) + parseFloat(this.velocity.x * dt);
+        console.log("Position after: " + this.position.x);
+        this.position.y += this.velocity.y * dt;
+        this.distance.x = SUN.position.x - this.position.x;
+        this.distance.y = SUN.position.y - this.position.y;
+        let distFromSun = Math.pow(this.distance.length(), 2)
+        console.log("Distance from sun:" + distFromSun)
+
+        let tmp = this.distance.normalize()
+        console.log("tmp.x: " + tmp.x + " tmp.y: " + tmp.y)
+        this.velocity.x = this.velocity.x + (this.gravConst * (SUN.mass / distFromSun) * tmp.x * dt);
+        this.velocity.y = this.velocity.y + (this.gravConst * (SUN.mass / distFromSun) * tmp.y * dt);
+        console.log("Velocity after stuff:" + this.velocity.x)
+    }
+
+    Setup(position, velocity, distance, mass, radius, color) {
+        this.position = position;
+        this.velocity = velocity;
+        this.distance = distance;
+        this.mass = mass;
+        this.radius = radius;
+        this.color = color;
+    }
+
+
 }
 
 window.addEventListener("load", () => {
@@ -81,17 +605,8 @@ window.addEventListener("resize", () => {
     UpdateCanvas()
 })
 
-var viewport = new Viewport()
-var drawableElements = []
-
-drawableElements.push(new Object(100,100,20))
-
-canvas.addEventListener("mousemove", MouseMove)
-canvas.addEventListener("wheel", MouseWheel)
-canvas.addEventListener('contextmenu', event => event.preventDefault());
-
 function MouseMove(event) {
-    switch(event.which) {
+    switch (event.which) {
         case 1:
             viewport.DragViewport(event)
             UpdateCanvas()
@@ -106,13 +621,45 @@ function MouseWheel(event) {
     UpdateCanvas()
 }
 
+function UpdateModel(SUN, model) {
+    drawableElements.forEach(e => {
+        e.Update(SUN, model)
+    })
+}
+
 function UpdateCanvas() {
     var context = canvas.getContext("2d")
-    var contextTransform =  context.getTransform()
+    var contextTransform = context.getTransform()
 
-    context.clearRect(0, 0, canvas.width/contextTransform.a, canvas.height/contextTransform.d)
+    context.clearRect(0, 0, canvas.width / contextTransform.a, canvas.height / contextTransform.d)
 
-    drawableElements.forEach( e => {
+    SUN.Draw(viewport);
+    drawableElements.forEach(e => {
         e.Draw(viewport)
     })
 }
+
+
+function mainLoop() {
+    UpdateModel(SUN, dtx);
+    UpdateCanvas();
+    requestAnimationFrame(mainLoop);
+}
+
+const dtx = 0.16;
+const gravitConst = 1000;
+let viewport = new Viewport()
+let drawableElements = []
+var SUN = new Planet(new Vector2D(500, 500), new Vector2D(100, 100), new Vector2D(0, 0), 1000, 50, "#ffff00", dtx, gravitConst)
+
+
+drawableElements.push(new Planet(new Vector2D(700, 50), new Vector2D(10, 50), new Vector2D(0, 0), 10, 13, "#ff0000", gravitConst))
+    //Tu się dzieją ciekawe rzeczy
+drawableElements.push(new Planet(new Vector2D(1000, 500), new Vector2D(10, 1), new Vector2D(0, 0), 10, 13, "#66ff99", gravitConst))
+drawableElements.push(new Planet(new Vector2D(1000, 1000), new Vector2D(-30, 30), new Vector2D(0, 0), 10, 13, "#0033cc", gravitConst))
+console.log("A co jest tutaj: " + drawableElements[0].position.x)
+
+canvas.addEventListener("mousemove", MouseMove)
+canvas.addEventListener("wheel", MouseWheel)
+canvas.addEventListener('contextmenu', event => event.preventDefault());
+requestAnimationFrame(mainLoop);
